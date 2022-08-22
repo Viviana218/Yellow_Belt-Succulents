157265e35479426788f848c26e285b97ef45ac61
Viviana Medina <vmedinaarango@gmail.com>
2022-08-22 17:57:44 -0500

    nuevo

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function eliminaCookie() {
+    var elementoCookie = document.querySelector('.cookie')
+    elementoCookie.remove();
+}
+
+function empty (btn1) {
+    alert("Your car is empty")
+}
+
+
+function onHover(){
+    document.getElementById("suc").src="images/succulents-2.jpg";
+}
+
+function offHover(){
+    document.getElementById("suc").src="images/succulents-1.jpg";
+}
